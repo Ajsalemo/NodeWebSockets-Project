126fb4b39764d2b86eaa727c8ea9b857fb72728b
Anthony Salemo <ajssalemo@gmail.com>
2021-11-27 11:32:05 -0500

    conditionally reacting to whether or not the client/server is connected to ws

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -3,6 +3,7 @@
     <h1>{{ msg }}</h1>
     <h3>Chat</h3>
     <button @click="connect">Connect to chat</button>
+    <input type="text" v-if="isConnected === true" />
   </div>
 </template>
 
@@ -14,11 +15,14 @@ export default {
   },
   methods: {
     connect() {
-      const url = "ws://localhost:3000";
+      const url = process.env.HOST_URL
+        ? `ws://${process.env.HOST_URL}`
+        : "ws://localhost:3000";
       const wsClient = new WebSocket(url);
 
       wsClient.onopen = () => {
         console.log(`Connection to ${url} has been established`);
+        this.isConnected = true;
       };
 
       wsClient.onmessage = (e) => {
@@ -29,6 +33,11 @@ export default {
         console.log(`WebSocket error: ${err}`);
       };
     },
+  },
+  data() {
+    return {
+      isConnected: false,
+    };
   },
 };
 </script>
