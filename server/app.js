e7028b5e5f3840a26cf343147746c55f6bfca8a8
Anthony Salemo <ajssalemo@gmail.com>
2021-11-26 22:05:49 -0500

    spun up a websockets server, scaffolded out a client/server project

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const ws = require("ws");
+const port = process.env.PORT || 3000;
+
+const socket = new ws.Server({ port })
+console.log(`Server listening on port: ${port}`)
+
+socket.on("connection", (socket) => {
+  socket.send("Hi client! You are now connected to me!");
+
+  socket.on("message", (message) => {
+    console.log(`New message from client: ${message}`);
+  });
+});
